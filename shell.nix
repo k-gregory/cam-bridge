29689c8f875cc85b70a24871f10ed894e765f211
Gregory Komar <xalvevs@gmail.com>
2024-06-01 11:02:52 +0000

    Fix shell.nix

--- shell.nix
+++ shell.nix
@@ -9,4 +9,8 @@
     gst_all_1.gstreamer  gst_all_1.gst-plugins-ugly  gst_all_1.gst-plugins-good  gst_all_1.gst-plugins-bad gst_all_1.gst-plugins-base glib
     stdenv.cc.cc.lib
   ]);
+  profile = ''
+    export A="${pkgs.libnice.out}"
+    export GST_PLUGIN_SYSTEM_PATH_1_0="${pkgs.gst_all_1.gst-plugins-base}/lib/gstreamer-1.0/:${pkgs.gst_all_1.gst-plugins-good}/lib/gstreamer-1.0/:${pkgs.gst_all_1.gst-plugins-bad}/lib/gstreamer-1.0/:${pkgs.gst_all_1.gst-plugins-ugly}/lib/gstreamer-1.0/:${pkgs.libnice.out}/lib/gstreamer-1.0"
+  '';
 }).env
